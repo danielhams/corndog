079c9f2aa03f26c8679c490207d5900e26ad99ae
Daniel Hams <daniel.hams@gmail.com>
2017-07-12 12:02:34 +0100

    Use the java "try with resources" to ensure the scanner is closed under all circumstances.

--- firstproject/src/com/rene/Main.java
+++ firstproject/src/com/rene/Main.java
@@ -1,68 +1,67 @@
 package com.rene;
+
 import java.util.Scanner;
 
-public class Main {
+public class Main
+{
 	public static void main( String[] args )
 	{
-		Scanner inputScanner = new Scanner( System.in );
-		
-		int x;
-		double averageoriginal = 0;
-		double averagemod1;
-		double lapoutaverage;
-
-		System.out.print( "how many laps to count : " );
-		x = inputScanner.nextInt();
-
-		double[] laptimes = new double[x];
-		double[] laptimemod = new double[x];
-
-		System.out.print( "Give limit for laps out of average : " );
-		lapoutaverage = inputScanner.nextDouble();
-
-		for( int i = 0 ; i < laptimes.length ; i++ )
+		try( Scanner inputScanner = new Scanner( System.in ) )
 		{
-			System.out.print( "Please enter Laptime " + (i + 1) + " : " );
-			laptimes[ i ] = inputScanner.nextDouble();
-		}
+			int x;
+			double averageoriginal = 0;
+			double averagemod1;
+			double lapoutaverage;
 
-		for( int i = 0 ; i < laptimes.length ; i++ )
-		{
-			averageoriginal = averageoriginal + laptimes[ i ];
-		}
+			System.out.print( "how many laps to count : " );
+			x = inputScanner.nextInt();
 
-		averageoriginal = averageoriginal / x;
-		System.out.println( "average : " + averageoriginal );
-		averagemod1 = averageoriginal;
+			double[] laptimes = new double[x];
+			double[] laptimemod = new double[x];
 
-		for( int repeat = 0 ; repeat < 4 ; repeat++ )
-		{
+			System.out.print( "Give limit for laps out of average : " );
+			lapoutaverage = inputScanner.nextDouble();
 
 			for( int i = 0 ; i < laptimes.length ; i++ )
 			{
-				if( laptimes[ i ] - averageoriginal > lapoutaverage
-						|| laptimes[ i ] - averageoriginal < -lapoutaverage )
-				{
-					laptimemod[ i ] = averagemod1;
-					System.out.println( laptimemod[ i ] );
-
-				}
-				else
-				{
-					laptimemod[ i ] = laptimes[ i ];
-					System.out.println( laptimemod[ i ] );
-				}
+				System.out.print( "Please enter Laptime " + (i + 1) + " : " );
+				laptimes[ i ] = inputScanner.nextDouble();
 			}
 
-			averagemod1 = 0;
 			for( int i = 0 ; i < laptimes.length ; i++ )
 			{
-				averagemod1 = (averagemod1 + laptimemod[ i ]);
-
+				averageoriginal = averageoriginal + laptimes[ i ];
 			}
-			averagemod1 = averagemod1 / x;
-			System.out.println( " average laptime mod : " + averagemod1 );
 
+			averageoriginal = averageoriginal / x;
+			System.out.println( "average : " + averageoriginal );
+			averagemod1 = averageoriginal;
+
+			for( int repeat = 0 ; repeat < 4 ; repeat++ )
+			{
+				for( int i = 0 ; i < laptimes.length ; i++ )
+				{
+					if( laptimes[ i ] - averageoriginal > lapoutaverage
+							|| laptimes[ i ] - averageoriginal < -lapoutaverage )
+					{
+						laptimemod[ i ] = averagemod1;
+						System.out.println( laptimemod[ i ] );
+					}
+					else
+					{
+						laptimemod[ i ] = laptimes[ i ];
+						System.out.println( laptimemod[ i ] );
+					}
+				}
+
+				averagemod1 = 0;
+				for( int i = 0 ; i < laptimes.length ; i++ )
+				{
+					averagemod1 = (averagemod1 + laptimemod[ i ]);
+				}
+				averagemod1 = averagemod1 / x;
+				System.out.println( " average laptime mod : " + averagemod1 );
+			}
 		}
 	}
 }
